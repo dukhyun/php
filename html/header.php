d28fcbdbe3de6f5062a771917c979ccbbecce38d
dukhyun <dackhean@gmail.com>
2016-06-22 14:21:58 +0900

    header.php 파일 수정

--- html/header.php
+++ html/header.php
@@ -1,14 +1,31 @@
-<?php // include ''; ?>
+<?php
+// $local_url = '';
+// $css_array['key'] = 'file';
+// include $local_url.'header.php';
+if (!$local_url) {
+    $local_url = '';
+}
+$nav_php = 'from/navybar.php';
+$css_array['main'] = 'style.css';
+$css_array['nav'] = 'from/nav.css';
+?>
 <!DOCTYPE HTML>
 <html>
 
 <head>
-<?php
+<?php // title
 if ($web_title != '') {
 	echo '</title>'.$web_title.'</title>';
+}
+?>
+<meta charset="UTF-8">
+<?php // css
+foreach ($css_array as $key => $file) {
+	echo '<link rel="stylesheet" type="text/css" href="'$local_url.$file.'">';
 }
 ?>
 </head>
 <body>
 <?php // navbar
+include $local_url.$nav_php;
 ?>
