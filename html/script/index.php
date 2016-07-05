de247b43dc16ee0d6f1e5b489296860358dff692
dukhyun <dackhean@gmail.com>
2016-07-05 13:11:30 +0900

    javascript

---
+++ html/script/index.php
@@ -0,0 +1,14 @@
+<?php
+$local_url = '../';
+$web_title = 'PHP ¿¬½ÀÀå';
+$nav_array = array();
+$nav_array[Home] = $local_url.'';
+include $_SERVER['DOCUMENT_ROOT'].'/../section/header.php';
+?>
+
+<script>
+var elements = Date();
+document.write(elements);
+</script>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/../section/footer.php'; ?>
