8768b452dbf6668d41464e146856b9ed7a400da2
dukhyun <dackhean@gmail.com>
2016-07-18 15:17:10 +0900

    검색

--- html/board/10_comment/index.php
+++ html/board/10_comment/index.php
@@ -30,16 +30,6 @@ include_once 'login.php';
 	$conn = db_connect(); // DB 접속 함수
 	$board_id = get_board_id($conn, 'board_test'); // 게시판 id
 	
-	// 게시판 갯수 구하기
-	$query = sprintf("SELECT count(*) AS count FROM post WHERE board_id = %d;", $board_id);
-	$result = mysqli_query($conn, $query);
-	if (!$result) {
-		echo $query.'<br>';
-		die ("Database access failed: ".mysqli_error());
-	}
-	$row = mysqli_fetch_assoc($result);
-	
-	$post_all = $row['count']; // 전체 글 갯수
 	$page_post = 10; // 한 페이지에 표시할 글의 갯수
 	$post_limit = ($page_post * $page) - $page_post; // 표시할 글 위치
 ?>
@@ -56,7 +46,12 @@ include_once 'login.php';
 		</ul>
 
 <?php
-	$query = sprintf("SELECT * FROM post WHERE board_id = %d ORDER BY id DESC LIMIT %d, %d;", $board_id, $post_limit, $page_post);
+	$temp_query = sprintf("board_id = %d", $board_id);
+	if (isset($_GET['search'])) {
+		$search_word = $_GET['search'];
+		$temp_query .= sprintf(" AND (title LIKE '%%%s%%' OR content LIKE '%%%s%%')", $search_word, $search_word);
+	}
+	$query = sprintf("SELECT * FROM post WHERE %s ORDER BY id DESC LIMIT %d, %d;", $temp_query, $post_limit, $page_post);
 	$result = mysqli_query($conn, $query);
 	if (!$result) {
 		echo $query.'<br>';
@@ -69,7 +64,7 @@ include_once 'login.php';
 			printf('<li class="title floatleft"><a href="view_post.php?page=%d&board_id=%d&post_id=%d">%s</a></li>'
 			, $page, $board_id, $row['id'], $row['title']);
 			if (isset($row['member_id'])) {
-				printf('<li class="author floatleft">%s</li>', $row['member_id']);
+				printf('<li class="author floatleft">%s</li>', get_member_name($conn, $row['member_id']));
 			} else {
 				printf('<li class="author floatleft">%s</li>', $row['author']);
 			}
@@ -77,6 +72,16 @@ include_once 'login.php';
 		echo '</ul>';
 	}
 	
+	// 게시판 갯수 구하기
+	$query = sprintf("SELECT count(*) AS count FROM post WHERE %s;", $temp_query);
+	$result = mysqli_query($conn, $query);
+	if (!$result) {
+		echo $query.'<br>';
+		die ("Database access failed: ".mysqli_error());
+	}
+	$row = mysqli_fetch_assoc($result);
+	
+	$post_all = $row['count']; // 전체 글 갯수
 	echo '<div class="table_caption">전체 : '.$post_all.'</div>';
 ?>
 
@@ -88,12 +93,17 @@ include_once 'login.php';
 	
 	<div class="fix page">
 <?php
+	$get_url = sprintf('index.php?');
+	if (isset($_GET['search'])) {
+		$get_url .= sprintf('search=%s&', $_GET['search']);
+	}
+
 	$page_all = ceil($post_all / $page_post); // 전체 페이지 수
 	$page_view = 5; // 한 페이지에 표시할 페이지 갯수
 	
 	$page_prev = (ceil($page / $page_view) - 1) * $page_view;
 	if ($page_prev) {
-		printf('<a href="index.php?page=%d">이전</a>', $page_prev);
+		printf('<a href="%spage=%d">이전</a>', $get_url, $page_prev);
 	}
 	
 	for ($i = ($page_prev + 1); $i <= ($page_prev + $page_view); $i += 1) {
@@ -102,15 +112,29 @@ include_once 'login.php';
 		} else if ($i == $page) {
 			printf('<span class="now">%d</span>', $i, $i);
 		} else {
-			printf('<a href="index.php?page=%d">%d</a>', $i, $i);
+			printf('<a href="%spage=%d">%d</a>', $get_url, $i, $i);
 		}
 	}
 	
 	$page_next = ceil($page / $page_view) * $page_view + 1;
 	if ($page_next < $page_all) {
-		printf('<a href="index.php?page=%d">다음</a>', $page_next);
+		printf('<a href="%spage=%d">다음</a>', $get_url, $page_next);
 	}
 ?>
+	</div>
+	
+	<!-- 검색 //-->
+	<div class="search">
+		<form action="search_process.php" method="post">
+		<ul>
+			<li class="floatleft">
+				<input type="text" name="search">
+			</li>
+			<li class="floatleft">
+				<input type="submit" value="검색">
+			</li>
+		</ul>
+		</form>
 	</div>
 </div>
 <?php mysqli_close($conn); ?>
